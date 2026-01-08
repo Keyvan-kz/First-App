09cbc36588da08e9a7f51a3f6c3fa5d39dc5e3ed
Your NameKeyvan <k1kzm29@gmail.com>
2026-01-08 12:36:31 +0330

    refactor resize component

--- src/components/detect-resize.jsx
+++ src/components/detect-resize.jsx
@@ -1,34 +1,30 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-function Resize () {
-    const[height , setHeight] = useState(window.innerHeight);
-    const[width , setWidth] = useState(window.innerWidth);
+function Resize() {
+  const [height, setHeight] = useState(window.innerHeight);
+  const [width, setWidth] = useState(window.innerWidth);
 
-    function detectResizeHeight () {
-        window.addEventListener('resize', () => setHeight(window.innerHeight)  )
-        console.log(height);
-        
-        return () => window.removeEventListener('resize', detectResizeHeight )
-    }
-    
-    function detectResizeWidth () {
-        window.addEventListener('resize', () => setWidth(window.innerWidth)  )
-        console.log(width);
-        
-        return () => window.removeEventListener('resize', detectResizeWidth)  
-    }
+  function detectResizeHeight() {
+    window.addEventListener("resize", () => setHeight(window.innerHeight));
+    return () => window.removeEventListener("resize", detectResizeHeight);
+  }
 
-    useEffect (detectResizeWidth , [width])
-    useEffect (detectResizeHeight , [height])
+  function detectResizeWidth() {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+    return () => window.removeEventListener("resize", detectResizeWidth);
+  }
 
-    return (
-        <div className="m-5 bg-orange-300 p-5 w-50 rounded text-center shadow-lg shadow-gray-800">
-        <h1>Height of Window</h1>
-        <p>{height}</p>
-        <h1>Width of Window</h1>
-        <p>{width}</p>
-        </div>
-    )
+  useEffect(detectResizeWidth, [width]);
+  useEffect(detectResizeHeight, [height]);
+
+  return (
+    <div className="m-5 bg-orange-300 p-5 w-50 rounded text-center shadow-lg shadow-gray-800">
+      <h1>Height of Window</h1>
+      <p>{height}</p>
+      <h1>Width of Window</h1>
+      <p>{width}</p>
+    </div>
+  );
 }
 
-export default Resize
+export default Resize;
